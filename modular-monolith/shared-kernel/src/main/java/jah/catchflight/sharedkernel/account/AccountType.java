c441475999bc6d648c3814ee2215a371552cf86a
The Programmer <java.architecture.hub@gmail.com>
2025-08-03 18:34:15 +0200

    Implement shared-kernel module

---
+++ modular-monolith/shared-kernel/src/main/java/jah/catchflight/sharedkernel/account/AccountType.java
@@ -0,0 +1,20 @@
+package jah.catchflight.sharedkernel.account;
+
+/**
+ * Defines the types of accounts available in the CatchFlight application.
+ * This enum represents the different account categories that users can have,
+ * such as standard or premium accounts, each with associated privileges or features.
+ *
+ * @since 1.0
+ */
+public enum AccountType {
+    /**
+     * Represents a standard account with basic features and access.
+     */
+    REGULAR,
+
+    /**
+     * Represents a premium account with enhanced features and privileges.
+     */
+    PREMIUM
+}
