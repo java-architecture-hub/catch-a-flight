cc115813ce5d565a25e7da6b51b9d72e62860b13
The Programmer <java.architecture.hub@gmail.com>
2025-08-03 17:43:13 +0200

    Add javadocs to domain package

--- modular-monolith/common/src/main/java/jah/catchflight/common/annotations/domain/package-info.java
+++ modular-monolith/common/src/main/java/jah/catchflight/common/annotations/domain/package-info.java
@@ -1,1 +1,13 @@
+/**
+ * Contains annotations used to mark classes and interfaces related to domain-driven design (DDD)
+ * concepts in the CatchFlight application.
+ * <p>
+ * This package provides annotations such as {@code @DomainAggregate}, {@code @DomainFactory},
+ * {@code @DomainPolicy}, {@code @DomainRepository}, {@code @DomainService}, and
+ * {@code @DomainValueObject} to clearly define the roles of domain components, promoting a
+ * consistent and structured approach to modeling the business domain.
+ * </p>
+ *
+ * @since 1.0
+ */
 package jah.catchflight.common.annotations.domain;
