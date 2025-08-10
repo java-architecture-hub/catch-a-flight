80f2eecdde814ce7a33e7f22498f88d1f1711cde
The Programmer <java.architecture.hub@gmail.com>
2025-08-10 16:17:52 +0200

    Check if account already exists

--- modular-monolith/account/src/main/java/jah/catchflight/account/adapter/in/SignInRestController.java
+++ modular-monolith/account/src/main/java/jah/catchflight/account/adapter/in/SignInRestController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static jah.catchflight.account.adapter.in.RestResources.ACCOUNT_API;
 import static jah.catchflight.account.adapter.in.RestResources.SIGN_IN_API;
 import static jah.catchflight.account.adapter.in.SignInRestController.SignInResponse.SuccessResponse;
 import static jah.catchflight.account.port.in.SignInUseCase.SignInCommand;
--- modular-monolith/account/src/main/java/jah/catchflight/account/domain/model/Password.java
+++ modular-monolith/account/src/main/java/jah/catchflight/account/domain/model/Password.java
@@ -2,8 +2,6 @@ package jah.catchflight.account.domain.model;
 
 import jah.catchflight.common.annotations.domain.DomainValueObject;
 
-import java.util.Objects;
-
 /**
  * A domain value object representing a user's password.
  * This record encapsulates the password string and ensures it is not null.
