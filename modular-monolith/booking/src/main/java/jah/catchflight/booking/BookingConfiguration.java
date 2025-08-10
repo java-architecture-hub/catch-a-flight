8c7d0b484a657c4ad805bbee663a4e61ec3ac396
java-architecture-hub <java.architecture.hub@gmail.com>
2025-08-10 02:03:17 +0200

    Add javadocs to configuration classes (#18)

--- modular-monolith/booking/src/main/java/jah/catchflight/booking/BookingConfiguration.java
+++ modular-monolith/booking/src/main/java/jah/catchflight/booking/BookingConfiguration.java
@@ -3,6 +3,12 @@ package jah.catchflight.booking;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * This class enables component scanning for the {@code jah.catchflight.booking} package,
+ * allowing Spring to automatically detect and register beans defined in that package.
+ * The {@code proxyBeanMethods} attribute is set to {@code false} to optimize performance
+ * by disabling proxying of bean methods, as method-level proxying is not required for this configuration.
+ */
 @Configuration(proxyBeanMethods = false)
 @ComponentScan("jah.catchflight.booking")
 public class BookingConfiguration {}
