1ee8ed23d0e28cecbf3932c19c31ffa5a1d41d9e
The Programmer <java.architecture.hub@gmail.com>
2025-08-10 03:35:30 +0200

    Add missing javadocs

--- modular-monolith/common/src/main/java/jah/catchflight/common/validation/InputValidationResult.java
+++ modular-monolith/common/src/main/java/jah/catchflight/common/validation/InputValidationResult.java
@@ -1,5 +1,10 @@
 package jah.catchflight.common.validation;
 
+/**
+ * A sealed interface representing the result of an input validation process.
+ * This interface defines two possible outcomes of validation: a successful validation
+ * ({@link Valid}) or an unsuccessful validation with an error message ({@link NotValid}).
+ */
 public sealed interface InputValidationResult {
     record Valid() implements InputValidationResult {}
     record NotValid(String message) implements InputValidationResult {}
