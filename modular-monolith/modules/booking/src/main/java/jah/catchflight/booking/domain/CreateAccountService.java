ba8f972a75bb6341bd3e7604b52afcb2d36872ed
java-architecture-hub <java.architecture.hub@gmail.com>
2025-08-13 23:25:57 +0200

    Fixed issue with duplicated bean name (#49)

--- modular-monolith/modules/booking/src/main/java/jah/catchflight/booking/domain/CreateAccountService.java
+++ modular-monolith/modules/booking/src/main/java/jah/catchflight/booking/domain/CreateAccountService.java
@@ -5,10 +5,12 @@ import jah.catchflight.common.annotations.domain.DomainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
 @DomainService
 @RequiredArgsConstructor
+@Service(CreateAccountService.BEAN_NAME)
 public class CreateAccountService implements CreateAccountUseCase {
+    public static final String BEAN_NAME = "bookingCreateAccountService";
+
     @Override
     public CreateAccountResult createUser(CreateAccountCommand command) {
         throw new UnsupportedOperationException();
