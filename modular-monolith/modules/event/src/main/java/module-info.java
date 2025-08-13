6770da5d43bf6e6d53c7f99b95a5144d10723f09
The Programmer <java.architecture.hub@gmail.com>
2025-08-13 23:56:01 +0200

    Added module-info.java to each module

---
+++ modular-monolith/modules/event/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module event {
+    requires common;
+    requires sharedkernel;
+    exports jah.catchflight.event.account;
+    exports jah.catchflight.event.query;
+}
---
+++ modular-monolith/modules/order/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module order {
+    requires common;
+    requires sharedkernel;
+    requires event;
+    requires static lombok;
+    requires spring.core;
+    requires spring.context;
+    requires spring.boot.autoconfigure;
+    requires spring.web;
+    requires org.apache.tomcat.embed.core;
+    requires org.slf4j;
+    exports jah.catchflight.order;
+}
