0cb1623dad0a908c95f45df017f4f969832a0a1c
java-architecture-hub <java.architecture.hub@gmail.com>
2025-08-15 23:39:13 +0200

    Add missing javadocs in booking module (#58)

--- modular-monolith/modules/booking/src/main/java/jah/catchflight/booking/domain/CreateAccountService.java
+++ modular-monolith/modules/booking/src/main/java/jah/catchflight/booking/domain/CreateAccountService.java
@@ -5,12 +5,30 @@ import jah.catchflight.common.annotations.domain.DomainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+/**
+ * A service responsible for handling user account creation in the system.
+ * <p>
+ * This class implements the {@link CreateAccountUseCase} interface to encapsulate the
+ * business logic for creating user accounts based on a provided command. It is annotated
+ * as a Spring {@code @Service} with a specific bean name and a domain service to indicate
+ * its role in the application layer and domain logic. Successful account creation may
+ * result in a {@link UserActivity#SIGN_IN} activity being recorded.
+ * </p>
+ *
+ * @see CreateAccountUseCase
+ * @see CreateAccountCommand
+ * @see CreateAccountResult
+ * @see UserActivity
+ */
 @DomainService
 @RequiredArgsConstructor
 @Service(CreateAccountService.BEAN_NAME)
 public class CreateAccountService implements CreateAccountUseCase {
     public static final String BEAN_NAME = "bookingCreateAccountService";
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public CreateAccountResult createUser(CreateAccountCommand command) {
         throw new UnsupportedOperationException();
