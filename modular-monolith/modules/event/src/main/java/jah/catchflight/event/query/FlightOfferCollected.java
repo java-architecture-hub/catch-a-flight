f784e1ad5f84f1839d4a72e080be30447e198d3f
The Programmer <java.architecture.hub@gmail.com>
2025-08-15 23:23:45 +0200

    Add missing javadocs in order event

--- modular-monolith/modules/event/src/main/java/jah/catchflight/event/query/FlightOfferCollected.java
+++ modular-monolith/modules/event/src/main/java/jah/catchflight/event/query/FlightOfferCollected.java
@@ -4,4 +4,13 @@ import jah.catchflight.common.events.DomainEvent;
 
 import java.util.UUID;
 
+/**
+ * A domain event representing the collection of a flight offer.
+ * <p>
+ * This record is used to signal that a flight offer has been successfully collected
+ * in an event-driven system. It includes a unique identifier for the event.
+ * </p>
+ *
+ * @param eventId the unique identifier of the event
+ */
 public record FlightOfferCollected(UUID eventId) implements DomainEvent {}
