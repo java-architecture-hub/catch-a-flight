922a674eee584581387ae44dcaa0625c75d72820
The Programmer <java.architecture.hub@gmail.com>
2025-08-15 23:19:38 +0200

    Add javadocs to domain model in order module

--- modular-monolith/modules/order/src/main/java/jah/catchflight/order/domain/OrderId.java
+++ modular-monolith/modules/order/src/main/java/jah/catchflight/order/domain/OrderId.java
@@ -5,9 +5,14 @@ import jah.catchflight.common.annotations.domain.DomainValueObject;
 import java.util.Objects;
 import java.util.UUID;
 
+/**
+ * A record representing a unique identifier for an order, using a UUID.
+ *
+ * @param id the UUID value of the order identifier, must not be null
+ */
 @DomainValueObject
 public record OrderId(UUID id) {
     public OrderId {
-        Objects.requireNonNull(id);
+        Objects.requireNonNull(id, "Order ID must not be null");
     }
 }
